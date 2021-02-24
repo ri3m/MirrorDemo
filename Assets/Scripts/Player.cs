1f667c1b23262a35f95ef3d3257ee359d1357f77
ri3m <ree13su@gmail.com>
2021-02-24 07:18:17 +0300

    hello world multiplayer edition

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Player : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+    void HandleMovement(){
+        //check if scipt is running on local player, bc we only want to use the players input for their movement, so that a player doesn't control someone else's movement 
+        if(isLocalPlayer){
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement =new Vector3(moveHorizontal *0.1f, moveVertical *0.1f, 0); //multipling to slow movement
+            transform.position=transform.position+movement;
+        }
+    }
+    
+}
